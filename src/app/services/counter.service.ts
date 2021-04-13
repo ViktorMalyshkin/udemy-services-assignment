6c50533c4d796f55a39569cca27d5a498cc86bd7
Viktor <malyshkin.viktor@icloud.com>
2021-04-13 18:49:08 +0300

    update CounterService
    the interaction of the service with the service must be added to the providers

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,8 +1,3 @@
-import {Injectable} from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
 export class CounterService {
   counterActiveToInactive: number = 0;
   counterInactiveToActive: number = 0;
