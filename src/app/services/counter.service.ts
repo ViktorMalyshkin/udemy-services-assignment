fc42fbf3c395d97f4170c453d110c489d6595463
Viktor <malyshkin.viktor@icloud.com>
2021-04-13 18:30:34 +0300

    add a CounterService
    add a CounterService which counts the number of active->inactive and inactive->active actions.

---
+++ src/app/services/counter.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CounterService {
+  counterActiveToInactive: number = 0;
+  counterInactiveToActive: number = 0;
+
+  constructor() { }
+
+  onCounterActiveToInactiveActions() {
+    ++this.counterActiveToInactive;
+    console.log('Active To Inactive actions: ', this.counterActiveToInactive);
+  }
+
+  onCounterInactiveToActiveActions() {
+    ++this.counterInactiveToActive;
+    console.log('Inactive To Active actions: ', this.counterInactiveToActive);
+  }
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,17 +7,17 @@ export class UserService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
-  // constructor(private counterService: CounterService) { }
+  constructor(private counterService: CounterService) { }
 
    onSetToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
-    // this.counterService.onCounterActiveToInactiveActions();
+    this.counterService.onCounterActiveToInactiveActions();
   }
 
   onSetToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
-    // this.counterService.onCounterInactiveToActiveActions();
+    this.counterService.onCounterInactiveToActiveActions();
   }
 }
