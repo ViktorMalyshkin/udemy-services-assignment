2418d5871f5af0452d481ec8d7fc499c9b09ea20
Viktor <malyshkin.viktor@icloud.com>
2021-04-13 18:29:57 +0300

    adding a UsersService
    Optimize this app by adding a UsersService which manages the active and inactive users

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,15 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Component} from '@angular/core'; import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
-  styleUrls: ['./active-users.component.css']
+  styleUrls: ['./active-users.component.css'],
 })
 export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+  users: string[];
+  // @Output() userSetToInactive = new EventEmitter<number>();
+
+  constructor(private userService: UserService) {
+    this.users = userService.activeUsers;
+  }
+
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.userService.onSetToInactive(id);
+    // this.userSetToInactive.emit(id);
   }
 }
--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core'; import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  styleUrls: ['./inactive-users.component.css'],
 })
 export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+  users: string[];
+  // userSetToActive = new EventEmitter<number>();
+  // private userService: UserService = new UserService();
+
+  constructor(private userService: UserService) {
+  this.users = userService.inactiveUsers;
+  }
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.userService.onSetToActive(id);
+    // this.userSetToActive.emit(id);
   }
 }
